e77d5c496291f3d98f6eda024dabdce53002c758
antonio Silva <antonios@uoregon.edu>
2023-03-14 19:29:48 -0700

    add form to add excercise, step 5

--- gym/journal/urls.py
+++ gym/journal/urls.py
@@ -6,5 +6,15 @@ urlpatterns = [
     path("", views.DatesListView.as_view(), name="index"),
     path("list/<int:list_id>/", views.ExerciseListView.as_view(), name="list"),
     path("list/add/", views.DateCreate.as_view(), name="list-add"),
+    path(
+        "list/<int:list_id>/item/add/",
+        views.ExerciseCreate.as_view(),
+        name="item-add",
+    ),
+    path(
+        "list/<int:list_id>/item/<int:pk>/",
+        views.ExerciseUpdate.as_view(),
+        name="item-update",
+    ),
 
 ]
--- gym/journal/views.py
+++ gym/journal/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.urls import reverse
 from django.views.generic import (
     ListView,
     CreateView,
@@ -12,6 +12,7 @@ class DatesListView(ListView):
     model = Date
     template_name = "journal/index.html"
 
+
 class ExerciseListView(ListView):
     model = Exercise
     template_name = "journal/date.html"
@@ -21,14 +22,62 @@ class ExerciseListView(ListView):
 
     def get_context_data(self):
         context = super().get_context_data()
-        context["date"] = Exercise.objects.get(id=self.kwargs["list_id"])
+        context["todo_list"] = Exercise.objects.get(id=self.kwargs["list_id"])
         return context
+
 
 class DateCreate(CreateView):
     model = Date
     fields = ["date"]
 
     def get_context_data(self):
-        context = super(ListCreate, self).get_context_data()
+        context = super(DateCreate, self).get_context_data()
         context["title"] = "Add a new date"
         return context
+
+
+class ExerciseCreate(CreateView):
+    model = Exercise
+    fields = [
+        "date",
+        "name",
+        "weight",
+        "sets",
+        "reps",
+    ]
+
+    def get_initial(self):
+        initial_data = super(ExerciseCreate, self).get_initial()
+        date_list = Date.objects.get(id=self.kwargs["list_id"])
+        initial_data["todo_list"] = date_list
+        return initial_data
+
+    def get_context_data(self):
+        context = super(ExerciseCreate, self).get_context_data()
+        date_list = Date.objects.get(id=self.kwargs["list_id"])
+        context["todo_list"] = date_list
+        context["title"] = "Enter a new exercise."
+        return context
+
+    def get_success_url(self):
+        return reverse("list", args=[self.object.todo_list_id])
+
+
+class ExerciseUpdate(UpdateView):
+    model = Exercise
+    fields = [
+        "date",
+        "name",
+        "weight",
+        "sets",
+        "reps",
+    ]
+
+    def get_context_data(self):
+        context = super(ExerciseUpdate, self).get_context_data()
+        context["todo_list"] = self.object.date
+        context["title"] = "Edit item"
+        return context
+
+    def get_success_url(self):
+        return reverse("list", args=[self.object.todo_list_id])
